209faf3cae8a8d2b59756d3966742ae953235927
Ali Jahanian <ali-design@csail.mit.edu>
2019-09-05 08:14:38 -0400

    args: mnist => imageSiz=28

--- dcgan_pytorch/main.py
+++ dcgan_pytorch/main.py
@@ -104,7 +104,7 @@ parser.add_argument('--dataset', required=True, help='cifar10 | lsun | mnist |im
 parser.add_argument('--dataroot', required=True, help='path to dataset')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
-parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
+parser.add_argument('--imageSize', type=int, default=28, help='the height / width of the input image to network')
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
@@ -120,6 +120,10 @@ parser.add_argument('--manualSeed', type=int, help='manual seed')
 parser.add_argument('--classes', default='bedroom', help='comma separated list of classes for the lsun data set')
 
 opt = parser.parse_args()
+
+if opt.dataset == 'mnist':
+    opt.imageSize = 28
+
 print(opt)
 
 try:
